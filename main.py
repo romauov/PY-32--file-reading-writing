58a8d2887eb2f4442bb6668ab525f6f4b50e4be9
Sergei Romanov <l2omanov@ya.ru>
2020-06-25 18:27:30 +0400

    help requested

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ with open('recipes.txt', encoding = 'utf8') as f:
             cook_book[meal_name].append(ing_dict)
         f.readline()
 
-print(cook_book)
+# print(cook_book)
 # print('\n', cook_book.keys())
-# print('\n', cook_book['Фахитос'])
+print('\n', cook_book['Омлет'])
 
 def make_buy_list(incoming_list, guest_number):
     """
@@ -34,20 +34,34 @@ def make_buy_list(incoming_list, guest_number):
             incoming_list.remove(meal)
             print(f'\nБлюдо {meal} отсутствует в книге рецептов')
     buy_dict = {}
+    i = 0
     for meal in incoming_list:
         ingredients = cook_book[meal]
+
         for ingredient in ingredients:
             if ingredient['ingredient_name'] not in buy_dict.keys():
                 ing_key = ingredient['ingredient_name']
+                # print(ing_key)
                 del(ingredient['ingredient_name'])
+                # print(ingredient)
                 buy_dict[ing_key] = ingredient
+                # print(buy_dict)
+                ingredient['ingredient_name'] = ing_key
+                print(i)
+                i += 1
 
             else:
                 # print(buy_dict[ingredient['ingredient_name']]['quantity']) #уже имеющееся кол-во ингридиента
                 # print(ingredient['quantity'])                              #кол-во которое нужно добавить
-                buy_dict[ingredient['ingredient_name']]['quantity'] += ingredient['quantity']
-                # print(buy_dict[ingredient['ingredient_name']]['quantity'])
-                # print(type(buy_dict[ingredient['ingredient_name']]['quantity']), type(ingredient['quantity']))
+                print(ingredient['quantity'])
+                print('--------------------------------------------')
+                amount_to_add = ingredient['quantity']
+                buy_dict[ingredient['ingredient_name']]['quantity'] += amount_to_add
+                print(ingredient['quantity'])
+                print('++++++++++++++++++++++++++++++++++++++++++++')
+                print(i)
+                i += 1
+
 
     for ingredient in buy_dict.keys():
         # print(ingredient)
@@ -59,8 +73,8 @@ def make_buy_list(incoming_list, guest_number):
     return buy_dict
 
 
-make_buy_list(['Запеченный картофель', 'Салат', 'Омлет', 'Фахитос', 'Шаурма'], 5)
-
+# make_buy_list(['Запеченный картофель', 'Салат', 'Омлет', 'Фахитос', 'Шаурма'], 5)
+make_buy_list(['Омлет', 'Омлет', 'Омлет', 'Омлет', 'Омлет', 'Омлет', 'Омлет', 'Омлет'], 1)
 
 
 
